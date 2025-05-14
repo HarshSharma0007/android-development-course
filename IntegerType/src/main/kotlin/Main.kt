be76d92ac60bd55066b183c84ce2f1a43d5403e1
Harsh Sharma <153315733+HarshSharma0007@users.noreply.github.com>
2025-05-14 21:56:44 +0530

    Integer Type
    Integer, Byte, Short and Long

---
+++ IntegerType/src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+//    val name = "Alex"
+//    val age = 23
+//    println("Hello $name your age is $age.")
+
+
+//  Explicitly defining the Int,Byte,Short & Long
+    val integerMaxValue = Int.MAX_VALUE
+    val integerMinValue = Int.MIN_VALUE
+    println("Integer Max Value: $integerMaxValue")
+    println("Integer Min Value: $integerMinValue")
+
+    val byteMaxValue: Byte = Byte.MAX_VALUE
+    val byteMinValue: Byte = Byte.MIN_VALUE
+    println("Byte Max Value: $byteMaxValue")
+    println("Byte Min Value: $byteMinValue")
+
+    val shortMaxValue: Short = Short.MAX_VALUE
+    val shortMinValue: Short = Short.MIN_VALUE
+    println("Short Max Value: $shortMaxValue")
+    println("Short Max Value: $shortMinValue")
+
+    // For Long
+    val myNumber = 28L
+    println("Long Number: $myNumber")
+
+}
